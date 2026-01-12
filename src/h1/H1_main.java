15479ffc48e88e9d536d40a0102297e1c518bbd9
brickdelivery <jburgma2@smail.uni-koeln.de>
2026-01-12 17:16:45 +0100

    add node structure

--- src/h1/H1_main.java
+++ src/h1/H1_main.java
@@ -2,7 +2,11 @@ package h1;
 
 public class H1_main {
     public static void main(String[] args) {
-
+        Node e = new Node(null);
+        Node d = new Node(e);
+        Node c = new Node(d);
+        Node b = new Node(c);
+        Node a = new Node(b);
     }
 
     public static int distance(Node x, Node y) {
